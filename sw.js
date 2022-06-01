e7112da242e89dd0d6c8d0abbf460e0f16d59faf
dogwonder <dogwonder@gmail.com>
2022-06-01 13:08:24 +0100

    update breadcrumbs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgwltd-0.324.0";
+const CACHE = "dgwltd-0.325.0";
 
 // This is the service worker with the Cache-first network
 const precacheFiles = [
@@ -97,7 +97,7 @@ self.addEventListener("activate", function(event) {
           })
         );
       }).then(function() {
-        console.log('[ServiceWorker] Claiming clients for version - 0.324.0');
+        console.log('[ServiceWorker] Claiming clients for version - 0.325.0');
         return self.clients.claim();
       })  
     );
