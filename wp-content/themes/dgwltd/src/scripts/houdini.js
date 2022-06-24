9fadb63bda7ae08fdf28a980279b9d912cda4ef4
dogwonder <dogwonder@gmail.com>
2022-06-24 14:20:07 +0100

    Change to triangles

--- wp-content/themes/dgwltd/src/scripts/houdini.js
+++ wp-content/themes/dgwltd/src/scripts/houdini.js
@@ -1,3 +1,4 @@
+//Via - https://css-tricks.com/creating-generative-patterns-with-the-css-paint-api/
 import random from "https://cdn.skypack.dev/random";
 import seedrandom from "https://cdn.skypack.dev/seedrandom";
 
@@ -16,7 +17,7 @@ class TinySpecksPattern {
     const { width, height } = geometry;
 
     const seed = props.get("--pattern-seed").value;
-    const colors = props.getAll("--pattern-colors").map((c) => c.toString());
+    const colors = props.getAll("--pattern-colors");
     const count = props.get("--pattern-speck-count").value;
     const minSize = props.get("--pattern-speck-min-size").value;
     const maxSize = props.get("--pattern-speck-max-size").value;
@@ -36,13 +37,26 @@ class TinySpecksPattern {
       ctx.rotate(((random.float(0, 360) * 180) / Math.PI) * 2);
       ctx.translate(-x, -y);
 
-      ctx.beginPath();
-      ctx.ellipse(x, y, radius, radius / 2, 0, Math.PI * 2, 0);
+      triangle(ctx, x, y, radius);
       ctx.fill();
+
+      // ctx.beginPath();
+      // ctx.ellipse(x, y, radius, radius / 2, 0, Math.PI * 2, 0);
+      // ctx.fill();
 
       ctx.restore();
     }
   }
+}
+
+function triangle(ctx, cx, cy, size) {
+  const originX = cx - size / 2;
+  const originY = cy - size / 2;
+  ctx.beginPath();
+  ctx.moveTo(originX, originY);
+  ctx.lineTo(originX + size, originY + size);
+  ctx.lineTo(originX, originY + size);
+  ctx.closePath();
 }
 
 if (typeof registerPaint !== "undefined") {
